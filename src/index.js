871401ca77b13966d8b58c275c33265be64726a1
vlaru <dn210690nvs@gmail.com>
2025-11-02 23:26:39 +0100

    refactor: add home directory setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import os from 'node:os';
 import {inputHandler} from './cli/index.js';
 import {messages} from './utility/messages.js';
 import process from 'node:process';
 import {getUser} from './user/getUser.js';
 import {exitHandler} from './utility/exitHandler.js';
+
+const homeDir = os.homedir();
+process.chdir(homeDir);
 
 const user = getUser();
 console.log(messages(user, 'hiUser'));
